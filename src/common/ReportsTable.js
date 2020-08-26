41b8a46b1d0b694f696430fc2378a99c1c5a1f2a
Astha Goel <Astha.Goel@meredith.com>
2020-08-26 14:06:54 +0530

    Reports  section

---
+++ src/common/ReportsTable.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function ReportsTable() {
+    return (
+        <div className="display_area col-md-11">
+            <div className="section-body">
+                <h5 className="head5 card-title">Entries</h5>
+                <table className="table table-striped">
+                    <thead >
+                        <tr className="table-headings_reports">
+                            <td>Date</td>
+                            <td>Task type</td>
+                            <td>Date</td>
+                            <td>Task type</td>
+                            <td>Date</td>
+                            <td>Task type</td>
+                            <td>Date</td>
+                            <td>Task type</td> 
+                            <td>Date</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default ReportsTable;
--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import ReportsEntry from '../common/ReportsEntry';
+import TimeSheetTable from '../common/ReportsTable';
 
 
 function Reports() {
     return (
-        <div className="report-wrapper">
-            <h1>Coming Soon</h1>
-        </div>
+        <div id="main" className="reports-wrapper">
+        <main className="col-sm-10 no_padding">
+            <div className="section_container timesheet">
+                <div className="row">
+                    <div className="col-md-11 no_padding">
+                        <TimeHeader />
+                        <div className="section_main white-bg">
+                            {/* Time Entry Section */}
+                            <ReportsEntry />
+                            {/*Time Sheet Entry  */}
+                            <TimeSheetTable />
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    </div>
     )
 }
+
+const TimeHeader = () => <div className="section_heading white-bg">
+    <h5 className="head5 col-md-4 left">
+        <span className="fa fa fa-files-o m-r-sm"></span>Reports</h5>
+</div>
 export default Reports;
