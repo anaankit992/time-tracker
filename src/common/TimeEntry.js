88e2cfb8a23ef3a2bb48c2b00398177856ea653a
Astha Goel <Astha.Goel@meredith.com>
2020-08-27 17:11:02 +0530

    CSS changes - Timesheet

--- src/common/TimeEntry.js
+++ src/common/TimeEntry.js
@@ -66,44 +66,44 @@ function TimeEntry(props) {
     return (
         <div className="col-md-12">
             <div className="section_row">
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="data">Select Date </label>
                     <input onChange={handleDateSelect} type="date" value={selections.date} className="form-control inputType" id="data"></input>
                 </div>
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="task">Task Type</label>
                     <SelectInput onChange={e => handleSelect(e, "taskType")} value={{ label: selections.taskType, value: selections.taskType }} isDisabled={!selections.date} options={taskType} handleChange={e => { console.log(e) }} />
                 </div>
 
             </div>
             <div className="section_row">
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="task">Task Name</label>
                     <SelectInput onChange={e => handleSelect(e, "taskName")} value={{ label: selections.taskName, value: selections.taskName }} isDisabled={!selections.taskType} options={taskNames[selections.taskType]} handleChange={e => { console.log(e) }} />
                 </div>
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="data">No of Hrs </label>
                     <input  onChange={handleHrsSelect} disabled={!selections.taskName} type="number" min="0" value={selections.hrs} className="form-control inputType" id="data"></input>
                 </div>
             </div>
             <div className="clear"></div>
             <div className="section_row">
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="task">Jira Ticket Number</label>
                     <input className="form-control inputType" id="tkt_no"></input>
                 </div>
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="data">Ticket Description</label>
                     <input className="form-control inputType" id="tkt_no"></input>
                 </div>
             </div>
             <div className="clear"></div>
             <div className="section_row">
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="task">Issue Type</label>
                     <input className="form-control inputType" id="tkt_no"></input>
                 </div>
-                <div className="col-sm-5 left">
+                <div className="col-sm-5 left div_nopadding">
                     <label for="data">Remarks</label>
                     <input className="form-control inputType" id="tkt_no"></input>
                 </div>
